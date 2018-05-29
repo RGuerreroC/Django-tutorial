e9944f7ae9da95f35dc133ad2b4be8498c2c8b99
Raul Guerrero <rguerrero@mgc.es>
2018-05-29 16:15:57 +0200

    Up to tutorial 3

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = '|| '.join([q.question_text for q in latest_question_list])
+    context = {'latest_question_list': latest_question_list }
 
-    return HttpResponse(output)
+    return render(request, 'polls/index', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("Estas viendo la pregunta %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/index', {'question':question})
 
 
 def results(request, question_id):
